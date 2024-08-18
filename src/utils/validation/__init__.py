c9e1135e6837fdf0813f7b253c9a3692e68713cd
BigOrangeQWQ <2284086963@qq.com>
2024-08-18 19:27:18 +0800

    fix: 修复相对引用问题

--- src/utils/validation/__init__.py
+++ src/utils/validation/__init__.py
@@ -1,19 +1,20 @@
 """验证数据是否符合规范"""
 
-from typing import Any, cast
+from typing import Any
 
 from pydantic import ValidationError
+
+from src.utils.store_test.models import DockerTestResult
 
 from .models import (
     AdapterPublishInfo,
     BotPublishInfo,
     PluginPublishInfo,
     PublishInfo,
-    Tag,
 )
 from .models import PublishType as PublishType
 from .models import ValidationDict as ValidationDict
-from .utils import color_to_hex, translate_errors
+from .utils import translate_errors
 
 validation_model_map: dict[PublishType, type[PublishInfo]] = {
     PublishType.BOT: BotPublishInfo,
@@ -34,14 +35,15 @@ def validate_info(
     validation_context = {
         "previous_data": raw_data.get("previous_data"),
         "skip_plugin_test": raw_data.get("skip_plugin_test"),
+        "plugin_test_output": raw_data.get("plugin_test_output"),
         "valid_data": {},
     }
 
     try:
-        data = dict(
-            validation_model_map[publish_type].model_validate(
-                raw_data, context=validation_context
-            )
+        data = (
+            validation_model_map[publish_type]
+            .model_validate(raw_data, context=validation_context)
+            .model_dump()
         )
         errors = []
     except ValidationError as exc:
@@ -51,22 +53,10 @@ def validate_info(
     # 翻译错误
     errors = translate_errors(errors)
 
-    # tags 会被转成 list[Tag]，需要转成 dict
-    if "tags" in data:
-        tags = cast(list[Tag], data["tags"])
-        data["tags"] = [
-            {
-                "label": tag.label,
-                "color": color_to_hex(tag.color),
-            }
-            for tag in tags
-        ]
-
     # 如果是插件，还需要额外验证插件加载测试结果
     if publish_type == PublishType.PLUGIN:
         skip_plugin_test = raw_data.get("skip_plugin_test")
         plugin_test_result = raw_data.get("plugin_test_result")
-        plugin_test_output = raw_data.get("plugin_test_output")
         plugin_test_metadata = raw_data.get("plugin_test_metadata")
 
         if plugin_test_metadata is None and not skip_plugin_test:
@@ -86,17 +76,6 @@ def validate_info(
             # 元数据缺失时，需要删除元数据相关的字段
             for key in metadata_keys:
                 data.pop(key, None)
-
-        if not skip_plugin_test and not plugin_test_result:
-            errors.append(
-                {
-                    "loc": ("plugin_test",),
-                    "msg": "插件无法正常加载",
-                    "type": "plugin_test",
-                    "ctx": {"output": plugin_test_output},
-                    "input": None,
-                }
-            )
 
     return {
         "valid": not errors,
